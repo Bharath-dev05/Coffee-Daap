f0d7d05438d7dd5594556c87d3c17db647c06bf2
Bharath <bharathsde05@gmail.com>
2025-05-25 10:56:32 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,15 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_URL =
-  " https://eth-sepolia.g.alchemy.com/v2/ZcVZd_XJe66Os77rvT8yrnQdw4ZzGAK5";
+const SEPOLIA_URL =
+  " https://eth-sepolia.g.alchemy.com/v2/ZcV*******Zd_XJe66Qdw4ZzGAK5";
 const PRIVATE_KEY =
-  "0820b63778dadae7924857a54e0a4b98d1dc4e507848e9fb5638d2dd7366fa3b";
+  "0820b63778dadae7924857a54e0a0*******7848e9fb5638d2dd7366fa3b";
 
-console.log(GOERLI_URL);
-console.log(PRIVATE_KEY);
-
-if (!GOERLI_URL || !PRIVATE_KEY) {
+if (!SEPOLIA_URL || !PRIVATE_KEY) {
   throw new Error("Please set your GOERLI_URL and PRIVATE_KEY in a .env file");
 }
 
@@ -19,7 +16,7 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     sepolia: {
-      url: GOERLI_URL,
+      url: SEPOLIA_URL,
       accounts: [PRIVATE_KEY],
     },
   },
